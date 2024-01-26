79488527d3c41829bec6b476eaf791fba05e0487
Stephan Bökelmann <sb@gruppe.ai>
2024-01-26 20:51:33 +0100

    Adds argparse
    This commit adds options to hand over a specific port and also to set --ext in order to expose the for external callers

--- app/tutorial.py
+++ app/tutorial.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+import argparse
+
+parser = argparse.ArgumentParser(description="Run the FastAPI server.")
+parser.add_argument("-p", "--port", type=int, default=8000, help="Port to run the server on. Default is 8000")
+parser.add_argument("--ext", action="store_true", help="Run the server on 0.0.0.0 instead of 127.0.0.1")
+
+args = parser.parse_args()
 
 app = FastAPI()
 
@@ -7,6 +14,5 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello World"}
 
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8008)
+    uvicorn.run("main:app", host="0.0.0.0" if args.ext else "127.0.0.1", port=args.port)
